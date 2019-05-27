1474b3259b5561bc5fab4cab7dd033167f56c6fe
nofadwiadelia <nofadwiadelia47@gmail.com>
2019-05-28 00:58:01 +0700

    PWA DONE

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Carousel } from 'antd';
 import "../components/Carousel.css";
 import Texty from 'rc-texty';
 import 'rc-texty/assets/index.css';
 import Typist from 'react-typist-updated';
 
-
-
 class MyCarousel extends React.Component{
     render() {
         return(
-            <Carousel autoplay>
+            <div className="bg" alt="bgr">
             <div align="left" >
             <div className="texty-demo" style={{ marginTop: 190 }}>
             <div style={{  padding: '50px' }}><h1><Texty>WELCOME</Texty></h1>
             <Typist>Bermimpilah dalam hidup, jangan hidup dalam bermimpi</Typist></div>
             </div>
             </div>
-            </Carousel>
+           </div>
         )
     }
 }
